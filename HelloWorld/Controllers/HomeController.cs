377aaad0ba279c3929b68f66a05fbcc350f3d961
Michael Bowen <mikee.bowen@gmail.com>
2020-07-02 17:04:07 -0700

    initial commit

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //int x = 1;
+            //x = x / (x - 1);
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View(productRepository.Products.First());
+        }
+
+        public ActionResult Products()
+        {
+            return View(productRepository.Products);
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
